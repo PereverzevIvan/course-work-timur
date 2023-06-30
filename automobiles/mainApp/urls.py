4c8f712145c1b90b3b867ea9922d1196da954ec9
ivan <peregh320@gmail.com>
2023-06-30 10:35:52 +0300

    Реализован функционал добававления и удаления комментариев к статьям

--- automobiles/mainApp/urls.py
+++ automobiles/mainApp/urls.py
@@ -4,6 +4,10 @@ from .views import *
 app_name = 'mainApp'
 urlpatterns = [
     path('', index, name='index'),
+    path('news/<int:page_no>/', show_all_news, name='all_news'),
+    path('one_article/<int:article_id>/', show_one_article, name='one_article'),
+    path('add_comment/<int:article_id>/', add_comment, name='add_comment'),
+    path('detele_comment/<int:comment_id>', delete_comment, name='delete_comment')
 ]
 
 handler404 = 'mainApp.views.error_404'
--- automobiles/mainApp/views.py
+++ automobiles/mainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponsePermanentRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,39 @@ def error_404(request, exception):
         'exception': exception
     }
     return render(request,'404.html', data)
+
+def show_all_news(request, page_no:int):
+    news = list(Article.objects.all()) * 100
+    return render(request, 'all_news.html', {'news': news})
+
+def show_one_article(request, article_id:int):
+    article = get_object_or_404(Article, pk=article_id)
+    comments = Comment.objects.filter(article_id=article_id)
+
+    return render(request, 'one_article.html', {'article': article, 'comments': comments})
+
+def add_comment(request, article_id:int):
+    if request.method == 'POST':
+        user = request.user
+        if user.is_authenticated and get_object_or_404(Article, pk=article_id):
+            comment = Comment()
+            comment.article_id = article_id
+            comment.author_id = user.id
+            comment.text = request.POST.get('comment-text')
+            comment.save()
+            return HttpResponsePermanentRedirect(reverse('mainApp:one_article', kwargs={'article_id': article_id}))
+        else:
+            return render(request, 'error.html', {'error_title': 'Ошибка добавления', 
+                                                  'error_text': 'Не удалось добавить комментарий'})
+        
+
+def delete_comment(request, comment_id:int):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    article_id = comment.article_id
+    user = request.user
+
+    if user.is_authenticated and comment.author_id == user.id:
+        comment.delete()
+        return HttpResponsePermanentRedirect(reverse('mainApp:one_article', kwargs={'article_id': article_id}))
+    return render(request, 'error.html', {'error_title': 'Ошибка удаления', 
+                                                  'error_text': 'Вы не можете удалить комментарий другого человека'})
